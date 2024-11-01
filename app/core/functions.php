8d7520a3e737bd11e7ec2ffe1a38e201bf0a652d
Ngoc <nt7nguyen@gmail.com>
2024-11-01 16:20:48 +0100

    Creating MVC

---
+++ app/core/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function show($stuff)
+{
+    echo "<pre>";
+    print_R($stuff);
+    echo "</pre>";
+}
+
---
+++ app/views/404.php
@@ -0,0 +1,3 @@
+Page Not Found
+
+Unfortunately the page you're looking for doesn't exist(anymore) or there was an error in the link you followed or typed.
