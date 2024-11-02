ca31acf9e33283fa841731bc1246d2cf69d96f12
Ngoc <nt7nguyen@gmail.com>
2024-11-02 20:54:28 +0100

    Home: Footer
    added footer and cv

--- app/views/portfolio/index.php
+++ app/views/portfolio/index.php
@@ -57,8 +57,16 @@
 
         <script src="<?= ASSETS ?>/portfolio/js/accordion.js"></script>
         
-        <footer>
-            
+        <footer class="center bottom">
+            <div class="fa-size social-media">
+                <a href="https://github.com/Catzu" target="_blank"><i class="fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/that-nguyen-b03434159/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://www.instagram.com/thatje_/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://x.com/aihoshinofan20" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+            </div>
+            <div class="sizeLocation">
+                Copyright © 2024 - Ngoc That Nguyen - All Rights Reserverd
+            </div>
         </footer>
     </body>
 </html>
