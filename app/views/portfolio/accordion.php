211290d731cfe84fcc19fac9955fee00e70a5be1
Ngoc <nt7nguyen@gmail.com>
2024-11-03 17:57:17 +0100

    Home: Skills finished and template build

--- app/views/portfolio/accordion.php
+++ app/views/portfolio/accordion.php
@@ -6,7 +6,91 @@
 
     <button class="accordion"><i class="fa-solid fa-book icon-color icon-space"></i>Skills</button>
     <div class="panel">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <!-- Card html -->
+        <div class="card-body"> 
+            <div class="card-1">
+                <div class="card-flex JSS"> <!-- Icon en title --> <!-- display flex so its next to each other --> 
+                    <div class="card-icon"><i class="fa-brands fa-html5"></i></div>
+                    <span class="card-title">HTML</span>
+                </div>
+                <a class="card-flex JSE" href="https://html.spec.whatwg.org/" target="_blank">
+                    <span>HTML</span> 
+                    <i class="fa-solid fa-link"></i>
+                </a>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+
+        <!-- Card css -->
+        <div class="card-body">
+            <div class="card-1">
+                <div class="card-flex JSS"> <!-- Icon en title --> <!-- display flex so its next to each other --> 
+                    <div class="card-icon"><i class="fa-brands fa-css3"></i></div>
+                    <span class="card-title">CSS</span>
+                </div>
+                <a class="card-flex JSE" href="https://www.w3.org/TR/CSS/#css" target="_blank">
+                    <span>CSS</span> 
+                    <i class="fa-solid fa-link"></i>
+                </a>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+
+        <!-- Card PHP -->
+        <div class="card-body">
+            <div class="card-1">
+                <div class="card-flex JSS"> <!-- Icon en title --> <!-- display flex so its next to each other --> 
+                    <div class="card-icon"><i class="fa-brands fa-php"></i></div>
+                    <span class="card-title">PHP</span>
+                </div>
+                <a class="card-flex JSE" href="https://www.php.net/" target="_blank">
+                    <span>PHP</span> 
+                    <i class="fa-solid fa-link"></i>
+                </a>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+
+        <!-- Card JS -->
+        <div class="card-body">
+            <div class="card-1">
+                <div class="card-flex JSS"> <!-- Icon en title --> <!-- display flex so its next to each other --> 
+                    <div class="card-icon"><i class="fa-brands fa-js"></i></div>
+                    <span class="card-title">JS</span>
+                </div>
+                <a class="card-flex JSE" href="https://www.javascript.com/" target="_blank">
+                    <span>JS</span> 
+                    <i class="fa-solid fa-link"></i>
+                </a>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+
+        <!-- Card MySQL -->
+        <div class="card-body">
+            <div class="card-1">
+                <div class="card-flex JSS"> <!-- Icon en title --> <!-- display flex so its next to each other --> 
+                    <div class="card-icon"><i class="fa-solid fa-database"></i></div>
+                    <span class="card-title">MySQL</span>
+                </div>
+                <a class="card-flex JSE" href="https://www.mysql.com/" target="_blank">
+                    <span>MySQL</span> 
+                    <i class="fa-solid fa-link"></i>
+                </a>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+
     </div>
 
     <button class="accordion"><i class="fa-solid fa-folder icon-color icon-space"></i>Projects</button>
--- app/views/portfolio/head.php
+++ app/views/portfolio/head.php
@@ -4,6 +4,7 @@
     <title><?php echo $data['page_title']?></title>
     <link rel="stylesheet" href="<?= ASSETS ?>/portfolio/css/index.css">
     <link rel="stylesheet" href="<?= ASSETS ?>/portfolio/css/accordion.css">
+    <link rel="stylesheet" href="<?= ASSETS ?>/portfolio/css/footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href='https://fonts.googleapis.com/css?family=Inter Tight' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
