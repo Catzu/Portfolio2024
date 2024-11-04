8d6e1d374f172ffe3a6d11a2327ff585f6140886
Ngoc <nt7nguyen@gmail.com>
2024-11-04 03:55:21 +0100

    form added names

--- app/views/portfolio/login.php
+++ app/views/portfolio/login.php
@@ -9,8 +9,8 @@
 
         <div class="responsiveness-upload">
             <form class="form-upload" method="post">
-                <input type="text" id="title" placeholder="Username" required>
-                <input type="password" id="title" placeholder="Password" required>
+                <input type="text" name="username" id="title" placeholder="Username" required>
+                <input type="password" name="password" id="title" placeholder="Password" required>
                 <input class="btn" type="submit" value="Login">
             </form>
         </div>
--- app/views/portfolio/upload.php
+++ app/views/portfolio/upload.php
@@ -9,16 +9,16 @@
 
         <div class="responsiveness-upload">
             <form class="form-upload">
-                <input type="text" id="title" placeholder="Enter title" required>
-                <input type="date" id="date" placeholder="DD/MM/YYYY" pattern="\d{2}/\d{2}/\d{4}" required>
+                <input type="text" name="title" id="title" placeholder="Enter title" required>
+                <input type="date" name="date" id="date" placeholder="DD/MM/YYYY" pattern="\d{2}/\d{2}/\d{4}" required>
                 <textarea id="description" placeholder="Enter description" required></textarea>
                 <div class="file">
                     <span>Image</span>
-                    <input type="file" id="image" accept="image/*" >
+                    <input type="file" name="image" id="image" accept="image/*" >
                 </div>
                 <div class="file">
                     <span>Video</span>
-                    <input type="file" id="video" accept="video/*">
+                    <input type="file" name="video" id="video" accept="video/*">
                 </div>
                 <input class="btn" type="submit" value="Submit">
             </form>
